37f08edbc3114d9f301fa0ba1ee0680b07560bb0
Mladen <mpenev13@gmail.com>
2020-03-20 21:59:37 +0200

    generalStructureChanges

---
+++ eCommerceAPI/eCommerceAPI/Models/OrderItems.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceAPI.Models
+{
+    public class OrderItems
+    {
+    }
+}
