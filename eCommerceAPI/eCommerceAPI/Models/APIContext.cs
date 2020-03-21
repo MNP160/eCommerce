2f4abe15685001630bad59ba319abc591be6dd65
Mladen <mpenev13@gmail.com>
2020-03-21 02:50:49 +0200

    fixing Context file bug

--- eCommerceAPI/eCommerceAPI/Models/APIContext.cs
+++ eCommerceAPI/eCommerceAPI/Models/APIContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eCommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,8 @@ namespace farmersAPi.Models
         public DbSet<Cathegory> Cathegory { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<Users> Users { get; set; }
-       
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<OrderItems> OrderItems { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -24,8 +26,10 @@ namespace farmersAPi.Models
             modelBuilder.Entity<Cathegory>().ToTable("Cathegories");
             modelBuilder.Entity<Product>().ToTable("Products");
             modelBuilder.Entity<Users>().ToTable("Users");
-            
-           
+            modelBuilder.Entity<Orders>().ToTable("Orders");
+            modelBuilder.Entity<OrderItems>().ToTable("OrderItems");
+
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
