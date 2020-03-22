4a57df1b6a24d0e319885d81abf21dc360685aa6
Bubval <lyubo.valkov@gmail.com>
2020-03-22 16:54:27 +0200

    added all api models

---
+++ eCommerceAPI/eCommerceFrontend/Models/REST/Objects/Orders/OrderItemsResponse.cs
@@ -0,0 +1,17 @@
+﻿using eCommerceFrontend.Models.REST.Objects.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceFrontend.Models.REST.Objects.Orders
+{
+    public class OrderItemsResponse
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public ProductResponse  Product { get; set; }
+        public int OrderId { get; set; }
+        public OrdersResponse Order { get; set; }
+    }
+}
---
+++ eCommerceAPI/eCommerceFrontend/Models/REST/Objects/User/UserResponse.cs
@@ -0,0 +1,20 @@
+﻿using eCommerceFrontend.Models.REST.Objects.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceFrontend.Models.REST.Objects
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public List<OrdersResponse> Orders { get; set; }
+    }
+}
