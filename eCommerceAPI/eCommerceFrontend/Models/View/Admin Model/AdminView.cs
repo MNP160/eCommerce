a0b090bb05464dc519ff0e840c571e52e3efaaa5
Bubval <lyubo.valkov@gmail.com>
2020-03-25 03:00:31 +0200

    admin pages - basic

---
+++ eCommerceAPI/eCommerceFrontend/Models/View/Admin Model/AdminView.cs
@@ -0,0 +1,21 @@
+﻿using eCommerceFrontend.Models.REST.Objects.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceFrontend.Models.View.Admin_Model
+{
+    public class AdminView
+    {
+        public List<OrdersResponse> FulfilledOrders = new List<OrdersResponse>();
+        public List<OrdersResponse> UnforefilledOrders = new List<OrdersResponse>();
+
+        public AdminView(List<OrdersResponse> fulfilledOrders, List<OrdersResponse> unfulfilledOrders)
+        {
+            FulfilledOrders = fulfilledOrders;
+            UnforefilledOrders = unfulfilledOrders;
+        }
+
+    }
+}
